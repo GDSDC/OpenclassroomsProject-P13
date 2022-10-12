aab9e9b76055555fd5e63c819f351b90c971ea90
GDSDC <gdc.business.2019@gmail.com>
2022-10-12 12:21:14 +0200

    refactor(views) : moving views to its application

--- oc_lettings_site/lettings/urls.py
+++ oc_lettings_site/lettings/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from oc_lettings_site import views
+from oc_lettings_site.lettings import views
 
 urlpatterns = [
     path('lettings/', views.lettings_index, name='lettings_index'),
--- oc_lettings_site/profiles/urls.py
+++ oc_lettings_site/profiles/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from oc_lettings_site import views
+from oc_lettings_site.profiles import views
 
 urlpatterns = [
     path('profiles/', views.profiles_index, name='profiles_index'),
