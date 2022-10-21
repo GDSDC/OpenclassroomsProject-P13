851c2eea76dda6011d2a6165ae182f517ab6eec9
GDSDC <gdc.business.2019@gmail.com>
2022-10-21 11:42:59 +0200

    fix(monitoring)

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,7 +5,6 @@ from django.urls import path, include
 def trigger_error(request):
     """ Function which will allow the error to be traced by Sentry """
     division_by_zero = 1 / 0
-    return 'division_by_zero'
 
 
 urlpatterns = [
