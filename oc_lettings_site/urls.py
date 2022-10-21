fd849257b7a8ecfbf42b02b0f1db8e5d198cef0b
GDSDC <gdc.business.2019@gmail.com>
2022-10-21 11:34:14 +0200

    feat(monitoring) : adding sentry path to urlpatterns

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+def trigger_error(request):
+    """ Function which will allow the error to be traced by Sentry """
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('', include('oc_lettings_site.web_site.urls')),
     path('', include('oc_lettings_site.lettings.urls')),
     path('', include('oc_lettings_site.profiles.urls')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
