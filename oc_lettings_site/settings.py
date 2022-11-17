afafb43c859c9ce5dd9d1cfb451b726039167e9f
GDSDC <gdc.business.2019@gmail.com>
2022-11-17 13:18:10 +0100

    refactor(settings) : hiding env variables

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,10 +1,10 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-
 from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
